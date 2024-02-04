8d2825ea4ec9ff6ea124aaccef9cd40f199d52c7
hannahbui <buithanhthao2004@gmail.com>
2024-02-04 17:09:18 +0700

    Updated a little CSS and Adding comment

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,3 +1,7 @@
+// created by Thanh Thao Bui - 104170172
+// This page is used for the content in the home page
+// I used reactstrap (React + Bootstrap) and CSS to style this page
+
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
@@ -10,9 +14,11 @@ const HeroSection = () => {
         <Row>
           <Col lg="12" md="12" sm="6">
             <div className="hero__content">
+              {/* the heading of the content  */}
               <h2>
                 <span>Coinmiya - Decentralized Trading System</span> 
               </h2>
+              {/* the introduction of the website */}
               <p>
                 Coinmiya is a decentralized NFT trading system, offering users a secure 
                 and transparent platform to trade Non-Fungible Tokens (NFT). Through 
@@ -23,10 +29,13 @@ const HeroSection = () => {
               </p>
 
               <div className="hero__btns d-flex align-items-center gap-4">
+                {/* the Explore button link to dashboard page */}
                 <button className="explore__btn d-flex align-items-center gap-2">
                   <i className="ri-rocket-line"></i>{" "}
                   <Link to="/dashboard">Explore</Link>
                 </button>
+
+                {/* the Create button link to create new product page */}
                 <button className="create__btn d-flex align-items-center gap-2">
                   <i className="ri-ball-pen-line"></i>
                   <Link to="/create">Create</Link>
