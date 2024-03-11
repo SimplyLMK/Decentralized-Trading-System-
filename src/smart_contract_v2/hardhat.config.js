14c8a15ad4245590d741798c2bd0cfb366b91d51
Sire Kha <exit>
2024-03-11 10:03:22 +0700

    new branch to accomodate SQL db and Fast API

--- src/smart_contract_v2/hardhat.config.js
+++ src/smart_contract_v2/hardhat.config.js
@@ -10,7 +10,6 @@ module.exports = {
     {
       url: "https://eth-sepolia.g.alchemy.com/v2/_cQb10zXZhy5PRgur8hYXZsvvlIpgKFr", // my alchemy depoyment app url
       accounts: ['1fd92d0f70352422cce7ddd1e6f54435e22bdf4fcaf5eac2c5a6dd77323b8c5c'] // my metamask private key
-
     }
   }
 };
